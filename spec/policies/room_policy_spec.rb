2895a71b70edab9e0873c3a6082c8abebc13fe00
Margarita Barvinok <brita@umich.edu>
2026-07-28 10:30:19 -0400

    fix room_policy spec

--- spec/policies/room_policy_spec.rb
+++ spec/policies/room_policy_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe RoomPolicy, type: :policy do
     subject { described_class.new({ user: user, role: "rover" }, room) }
 
     it { is_expected.to forbid_actions(%i[is_admin is_developer is_readonly index show create new archive unarchive destroy]) }
-    it { is_expected.to permit_only_actions(%i[is_rover]) }
+    it { is_expected.to permit_only_actions(%i[is_rover delete_image upload_image]) }
   end
 
   context 'with readonly role' do
     subject { described_class.new({ user: user, role: "readonly" }, room) }
 
-    it { is_expected.to forbid_actions(%i[is_rover is_developer is_admin index new create archive unarchive destroy]) }
+    it { is_expected.to forbid_actions(%i[is_rover is_developer is_admin index new create archive unarchive destroy delete_image upload_image]) }
     it { is_expected.to permit_only_actions(%i[is_readonly]) }
   end
 
@@ -22,14 +22,14 @@ RSpec.describe RoomPolicy, type: :policy do
     subject { described_class.new({ user: user, role: "admin" }, room) }
 
     it { is_expected.to forbid_actions(%i[is_rover is_developer is_readonly]) }
-    it { is_expected.to permit_only_actions(%i[is_admin index show create new archive unarchive destroy]) }
+    it { is_expected.to permit_only_actions(%i[is_admin index show create new archive unarchive destroy delete_image upload_image]) }
   end
 
   context 'with developer role' do
     subject { described_class.new({ user: user, role: "developer" }, room) }
 
     it { is_expected.to forbid_actions(%i[is_rover is_readonly]) }
-    it { is_expected.to permit_only_actions(%i[is_admin is_developer index show create new archive unarchive destroy]) }
+    it { is_expected.to permit_only_actions(%i[is_admin is_developer index show create new archive unarchive destroy delete_image upload_image]) }
   end
 
   context 'with no role' do
